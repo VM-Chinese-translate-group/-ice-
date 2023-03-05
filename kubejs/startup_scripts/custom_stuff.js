0c0abfc68edf6b77a873811afd6cc5cdf8c6d2be
asd. sad <13319932858@163.com>
2023-03-05 16:43:32 +0800

    kjs自定义汉化

--- kubejs/startup_scripts/custom_stuff.js
+++ kubejs/startup_scripts/custom_stuff.js
@@ -1,34 +1,34 @@
 //Blocks
 onEvent('block.registry', event => {
   //Frozen Wood
-  event.create('frozen_oak').material('wood').hardness(2).displayName('Frozen Oak')
+  event.create('frozen_oak').material('wood').hardness(2).displayName('冰封橡木')
 })
 
 //Items
 onEvent('item.registry', event => {
 
 //Uncompleted Time in a Bottle
-event.create('un_tiab').displayName('Uncompleted Time in a Bottle')
+event.create('un_tiab').displayName('未完成的时间之瓶')
 //Gelid Enderium Ingot
-event.create('gelid_enderium_ingot').displayName('Gelid Enderium Ingot')
+event.create('gelid_enderium_ingot').displayName('极寒末影锭')
 //Gelid Processor
-event.create('gelid_processor').displayName('Gelid Processor')
+event.create('gelid_processor').displayName('极寒处理器')
 //Dielectric Mixture
-event.create('dielectric_mixture').displayName('Dielectric Mixture')
+event.create('dielectric_mixture').displayName('介质混合物')
 //Void Metal
-event.create('void_metal').displayName('Void Metal')
+event.create('void_metal').displayName('虚空金属')
 //Freezed Nitro Ingot
-event.create('nitrozed_ingot').displayName('Freezed Nitro Ingot')
+event.create('nitrozed_ingot').displayName('冷冻硝基锭')
 //Condensed Energy
-event.create('ce').displayName('Condensed Energy')
+event.create('ce').displayName('浓缩能量')
 })
 
 //Fluids
 onEvent('fluid.registry', event => {
   //Liquid Aura
-  event.create('liquid_aura').textureThick(0x258503).bucketColor(0x258503).displayName('Liquid Aura')
+  event.create('liquid_aura').textureThick(0x258503).bucketColor(0x258503).displayName('液体灵气')
   //Gelid Cryotheum
-  event.create('gelid_cryotheum').textureThick(0x73deff).bucketColor(0x73deff).displayName('Gelid Cryotheum')
+  event.create('gelid_cryotheum').textureThick(0x73deff).bucketColor(0x73deff).displayName('极寒之凛冰')
   //Dielectric Liquid
-  event.create('dielectric_liquid').textureThick(0x212121).bucketColor(0x212121).displayName('Dielectric Liquid')
+  event.create('dielectric_liquid').textureThick(0x212121).bucketColor(0x212121).displayName('液体介质')
 })
